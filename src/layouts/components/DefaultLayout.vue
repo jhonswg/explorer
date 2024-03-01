0a465363520cfd93ecd6a35bfb27e5d8a6652444
Dwentzart <118625308+Dwentzart@users.noreply.github.com>
2024-03-01 12:58:01 +0000

    modified:   chains/mainnet/blockx.json 	new file:   chains/mainnet/entrypoint.json 	new file:   chains/mainnet/mantra.json 	new file:   chains/mainnet/selfchain.json 	new file:   chains/mainnet/sidechain.json 	modified:   index.html 	modified:   public/favicon.ico 	modified:   public/logo.svg 	new file:   public/logos/entrypoint.png 	new file:   public/logos/mantra.png 	new file:   public/logos/selfchain.png 	new file:   public/logos/side.png 	modified:   src/assets/logo.svg 	modified:   src/layouts/components/DefaultLayout.vue

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -69,7 +69,7 @@ function selected(route: any, nav: NavLink) {
         <RouterLink to="/" class="flex items-center">
           <img class="w-10 h-10" src="../../assets/logo.svg" />
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+            Explorer™
           </h1>
         </RouterLink>
         <div
@@ -276,7 +276,7 @@ function selected(route: any, nav: NavLink) {
 
         <div class="px-4 text-sm pt-2 text-gray-400 pb-2 uppercase">{{ $t('module.links') }}</div>
         <a
-          href="https://twitter.com/ping_pub"
+          href="https://twitter.com/dwentz_"
           target="_blank"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
@@ -289,7 +289,7 @@ function selected(route: any, nav: NavLink) {
         </a>
         <a
           v-if="showDiscord"
-          href="https://discord.com/invite/CmjYVSr6GW"
+          href="https://discord.com/"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
@@ -301,7 +301,7 @@ function selected(route: any, nav: NavLink) {
           </div>
         </a>
         <a
-          href="https://github.com/ping-pub/explorer/discussions"
+          href="https://github.com/Dwentzart"
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
